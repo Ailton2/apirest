968c9604c5e1b6c51b521cbd4f33408bb8fe1d59
Ailton <tym.ailton@gmail.com>
2021-02-02 13:56:51 -0300

    resfull postmapping usuario

--- src/main/java/br/com/apirest/controller/UsuarioController.java
+++ src/main/java/br/com/apirest/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,14 @@ public class UsuarioController {
 		 return new ResponseEntity<Usuario>(user,HttpStatus.OK);
 	}
 	
+	@PutMapping(value = "/" ,produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario){
+		
+		Usuario user =repositoryUsuario.save(usuario);
+		
+		return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+		
+	}
 
 	
 }
