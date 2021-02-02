42622abd2f290dc79e77006b190c80e39243431a
Ailton <tym.ailton@gmail.com>
2021-02-02 12:40:44 -0300

    metodo restfull para salvar usuarios

--- src/main/java/br/com/apirest/controller/UsuarioController.java
+++ src/main/java/br/com/apirest/controller/UsuarioController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +40,14 @@ public class UsuarioController {
 		
 		List<Usuario> usuarios = (List<Usuario>) repositoryUsuario.findAll();
 		return new ResponseEntity<>(usuarios, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/" ,produces = "application/json")
+	public ResponseEntity<Usuario> savarUsuario(@RequestBody Usuario usuario){
+		
+		 Usuario user = repositoryUsuario.save(usuario);
+		 
+		 return new ResponseEntity<Usuario>(user,HttpStatus.OK);
 	}
 	
 
