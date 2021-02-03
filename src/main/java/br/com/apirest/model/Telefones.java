b4faa01be774cbcc57925ddd5cfc6e7d253df3b9
Ailton <tym.ailton@gmail.com>
2021-02-02 21:26:18 -0300

    adicionando o obj telefone

---
+++ src/main/java/br/com/apirest/model/Telefones.java
@@ -0,0 +1,49 @@
+package br.com.apirest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Telefones {
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String numero;
+	
+	@ManyToOne
+	@org.hibernate.annotations.ForeignKey(name = "usuario_id")
+	private Usuario usuario;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
+
+}
