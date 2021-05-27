541fe6191b0e051df3498e6d89292277478639ac
Ailton2 <tym.ailton@hotmail.com>
2021-05-26 21:30:38 -0300

    adicionando o userdatails no usuario e criando a tabela de roles

--- src/main/java/br/com/apirest/model/Usuario.java
+++ src/main/java/br/com/apirest/model/Usuario.java
@@ -1,18 +1,30 @@
 package br.com.apirest.model;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
+import javax.persistence.UniqueConstraint;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
 	
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
@@ -21,9 +33,19 @@ public class Usuario {
 	private String login;
 	private String senha;
 	
-	@OneToMany(mappedBy = "usuario",orphanRemoval = true,cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "usuario",orphanRemoval = true,cascade = CascadeType.ALL,fetch = FetchType.LAZY)
 	private List<Telefones> telefones =new ArrayList<Telefones>();
 	
+	@OneToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "usuario_role",uniqueConstraints = 
+	@UniqueConstraint(columnNames = {"usuario_id","role_id"},
+	name = "unique_role_user"),joinColumns = @JoinColumn(name = "usuario_id",
+	referencedColumnName = "id", table = "usuario", unique = false,
+	foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
+	inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id", 
+	table = "role", unique = false, updatable = false,
+	foreignKey = @ForeignKey(name = "role_fk", value = ConstraintMode.CONSTRAINT)))
+	private List<Role> roles;
 	
 	public List<Telefones> getTelefones() {
 		return telefones;
@@ -54,6 +76,36 @@ public class Usuario {
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles;
+	}
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+	@Override
+	public String getUsername() {
+		return this.login;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 	
 	
